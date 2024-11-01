640c59ec23a2c60ff52feec35c3bd201e2e57ca7
Elshan-Dunyamaliyev <elshan.dunyamaliyev@aist.group>
2024-11-01 12:51:56 +0400

    final commit

--- src/main/java/dev/elshan/urlShortener/repository/UrlRepository.java
+++ src/main/java/dev/elshan/urlShortener/repository/UrlRepository.java
@@ -3,5 +3,9 @@ package dev.elshan.urlShortener.repository;
 import dev.elshan.urlShortener.model.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UrlRepository extends JpaRepository<Url,Long> {
+
+    Optional<Url> findByShortenedUrl(String shortenedUrl);
 }
--- src/main/java/dev/elshan/urlShortener/service/UrlService.java
+++ src/main/java/dev/elshan/urlShortener/service/UrlService.java
@@ -1,4 +1,10 @@
 package dev.elshan.urlShortener.service;
 
+import dev.elshan.urlShortener.model.Url;
+import org.springframework.web.servlet.view.RedirectView;
+
 public interface UrlService {
+
+    RedirectView findBaseUrlByShortenedUrl(String url);
+    void convertToShortUrl(String longUrl);
 }
